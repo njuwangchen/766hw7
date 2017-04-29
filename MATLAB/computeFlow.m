bc9bb5ce1211092b52c1361a7acd249b4baa45d9
Chen Wang <chenwang@reksai.cs.wisc.edu>
2017-04-29 18:05:42 -0500

    initial commit

---
+++ MATLAB/computeFlow.m
@@ -0,0 +1,66 @@
+function result = computeFlow(img1, img2, win_radius, template_radius, grid_MN)
+
+row_num = grid_MN(1);
+col_num = grid_MN(2);
+
+row_space = size(img1, 1)/(row_num);
+col_space = size(img1, 2)/(col_num);
+
+x = [];
+y = [];
+u = [];
+v = [];
+
+for i = row_space:row_space:size(img1, 1)
+    for j = col_space:col_space:size(img1, 2)
+        row_ind = i-row_space/2;
+        col_ind = j-col_space/2;
+        
+        template = img1(row_ind-template_radius:row_ind+template_radius, ...
+            col_ind-template_radius:col_ind+template_radius);
+        window = img2(row_ind-win_radius:row_ind+win_radius, ...
+            col_ind-win_radius:col_ind+win_radius);
+        
+        c = normxcorr2(template, window);
+        [row_peak, col_peak] = find(c==max(c(:)));
+        row_peak = row_peak(1);
+        col_peak = col_peak(1);
+        row_crp = row_ind-win_radius+row_peak;
+        col_crp = col_ind-win_radius+col_peak;
+        
+        x = [x; col_ind];
+        y = [y; row_ind];
+        u = [u; col_crp-col_ind];
+        v = [v; row_crp-row_ind];
+        
+    end
+end
+
+fh = figure();
+imshow(img1)
+hold;
+quiver(x, y, u, v)
+
+result = saveAnnotatedImg(fh);
+
+function annotated_img = saveAnnotatedImg(fh)
+figure(fh); % Shift the focus back to the figure fh
+
+% The figure needs to be undocked
+set(fh, 'WindowStyle', 'normal');
+
+% The following two lines just to make the figure true size to the
+% displayed image. The reason will become clear later.
+img = getimage(fh);
+truesize(fh, [size(img, 1), size(img, 2)]);
+
+% getframe does a screen capture of the figure window, as a result, the
+% displayed figure has to be in true size. 
+frame = getframe(fh);
+frame = getframe(fh);
+pause(0.5); 
+% Because getframe tries to perform a screen capture. it somehow 
+% has some platform depend issues. we should calling
+% getframe twice in a row and adding a pause afterwards make getframe work
+% as expected. This is just a walkaround. 
+annotated_img = frame.cdata;
---
+++ MATLAB/trackingTester.m
@@ -0,0 +1,64 @@
+function trackingTester(data_params, tracking_params)
+
+first_img = imread(fullfile(data_params.data_dir,...
+    data_params.genFname(data_params.frame_ids(1))));
+xmin = tracking_params.rect(1);
+ymin = tracking_params.rect(2);
+width = tracking_params.rect(3);
+height = tracking_params.rect(4);
+
+sub_img = first_img(ymin:ymin+height-1, xmin:xmin+width-1, :);
+
+[index_sub_img, map] = rgb2ind(sub_img, tracking_params.bin_n);
+[color_hist, edges] = histcounts(index_sub_img, tracking_params.bin_n);
+
+for i = 2: size(data_params.frame_ids, 2)
+    cur_img = imread(fullfile(data_params.data_dir,...
+    data_params.genFname(data_params.frame_ids(i))));
+    
+    half_w = tracking_params.search_half_window_size;
+    search_area_xmin = xmin-half_w;
+    search_area_ymin = ymin-half_w;
+    search_width = width + 2*half_w;
+    search_height = height + 2*half_w;
+    
+    search_area = cur_img(search_area_ymin:search_area_ymin+search_height-1,...
+        search_area_xmin:search_area_xmin+search_width-1, :);
+    search_col = im2col_rgb(search_area, [height width]);
+        
+    hist_mat = zeros(size(search_col, 2), tracking_params.bin_n);
+    corr_vec = [];
+    for j = 1: size(search_col, 2)
+        index_cur_window = rgb2ind(search_col(:, j, :), map);
+        [N, E] = histcounts(index_cur_window, tracking_params.bin_n);
+        hist_mat(j, :) = N;
+        corr_vec = [corr_vec; correlation(color_hist, N)];
+    end
+    
+    max_corr = max(corr_vec)
+    max_ind = find(corr_vec == max_corr)
+    
+    move = search_height - height + 1;
+    xmin = search_area_xmin + int32(max_ind/move);
+    ymin = search_area_ymin + mod(max_ind, move);
+    
+%     [xmin ymin width height]
+%     [search_area_xmin search_area_ymin search_width search_height]
+    
+    box_img = drawBox(cur_img, [xmin ymin width height], [255 0 0], 3);
+%     box_img2 = drawBox(box_img, [search_area_xmin search_area_ymin search_width search_height], [0 255 0], 3);
+    figure, imshow(box_img);
+    
+end
+
+function B = im2col_rgb(img, sz, varargin)
+B = cell(1,size(img,3));
+for i=1:size(img,3)
+    B{i} = im2col(img(:,:,i), sz, varargin{:});
+end
+B = cat(3, B{:});
+
+function C = correlation(A, B)
+A_bar = mean(A);
+B_bar = mean(B);
+C = sum((A-A_bar).*(B-B_bar)) / (sqrt(sum((A-A_bar).^2))*sqrt(sum((B-B_bar).^2)));
